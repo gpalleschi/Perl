6fb3b744db4a92ece0a1c0aa70cf3a5bf1df7676
gpalleschi <gpalleschi@gmail.com>
2015-05-24 18:38:59 +0200

    First Commit

---
+++ stat.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl -w
+
+#
+# stat.pl 
+#
+#
+# This script execute a stat of a file in UNIX/LINUX environment
+#
+
+
+($dev, $ino, $mode, $nlink, $uid, $gid, $rdev, $size,
+
+    $atime, $mtime, $ctime, $blksize, $blocks) = stat($ARGV[0]);
+
+ 
+
+print("dev     = $dev\n");
+
+print("ino     = $ino\n");
+
+print("mode    = $mode\n");
+
+print("nlink   = $nlink\n");
+
+print("uid     = $uid\n");
+
+print("gid     = $gid\n");
+
+print("rdev    = $rdev\n");
+
+print("size    = $size\n");
+
+print("atime   = $atime\n");
+
+print("mtime   = $mtime\n");
+
+print("ctime   = $ctime\n");
+
+print("blksize = $blksize\n");
+
+print("blocks  = $blocks\n");
+
+ 
+
+($sec, $min, $hr, $day, $month, $year, $day_Of_Week, $julianDate, $dst) = localtime($ctime);
+
+$year = $year + 1900;
+
+$month = $month + 1;
+
+printf(" ctime $year/$month/$day $hr:$min:$sec\n");
+
+($sec, $min, $hr, $day, $month, $year, $day_Of_Week, $julianDate, $dst) = localtime($mtime);
+
+$year = $year + 1900;
+
+$month = $month + 1;
+
+printf(" mtime $year/$month/$day $hr:$min:$sec\n");
+
+($sec, $min, $hr, $day, $month, $year, $day_Of_Week, $julianDate, $dst) = localtime($atime);
+
+$year = $year + 1900;
+
+$month = $month + 1;
+
+printf(" atime $year/$month/$day $hr:$min:$sec\n");
+
+ 
